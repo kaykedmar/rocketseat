26735b4face67929f8e330b7f22265140458e2e0
Kayk eli <kayksantana133@gmail.com>
2023-10-27 22:03:31 -0300

    att

---
+++ math-random/script.js
@@ -0,0 +1,14 @@
+let result = Number(prompt("Adivinhe um numero de 0 a 10"))
+const RANDOMNUMBER = Math.round(Math.random() * 10)
+const MATCH = result == RANDOMNUMBER 
+
+let xTentativas = 0; 
+
+while(result != RANDOMNUMBER ) { 
+  result = prompt(('Error: Tente novamente:'))
+  xTentativas++
+}
+
+alert(`Parabens, Voce acertou!! em ${xTentativas} Tentivas:`)
+
+// console.log(RESULT, RANDOMNUMBER, MATCH)
