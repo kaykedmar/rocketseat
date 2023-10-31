5588e3cd6a5c3c712206f1a69e927399fb8212aa
Kayk eli <kayksantana133@gmail.com>
2023-10-31 20:19:03 -0300

    att

--- menu-opcao2/script.js
+++ menu-opcao2/script.js
@@ -1,53 +1,58 @@
-let option;
-let items = []; 
+    let option;
+    let items = [];
 
-while (option != 4) { 
+    while (option != 4) {
+      option = Number(prompt(`Olá usuário! Digite o número da opção abaixo:
 
-  option = Number(prompt(`Olá usúario! Digite o numero da opcao abaixo
+        1. Cadastrar um item na lista
+        2. Mostrar itens cadastrados
+        3. Retirar item
+        4. Sair do programa`));
 
-  1. Cadastrar um item na lista
-  2. Mostrar itens cadastrados
-  3. Retirar item
-  4. Sair do programa` ))
+  switch (option) {
+    case 1:
 
+      let item = prompt('Digite o nome do item a ser cadastrado');
+      items.push(item);
 
-  switch(option) { 
-    case 1: 
-    let item =  prompt('Digite o nome do Item a ser cadastrado')
-    items.push(item)
-    break
+      if (item === '') {
+        alert('Cadastro invalido:')
+      }
+      break;
 
-    case 2: 
-    
-    if (items.length == 0) { 
-      alert('Nenhum item cadastrado') 
-    } 
-    else { 
-      alert(items)
-    }
-    break
-    case 3: 
-    if (items.length === 0) { 
-      alert('Nenhum item para retirar.') 
-    }
-     else { 
-        alert('Item Retirado:')
-        items.pop(items)
-    }
-    break
+    case 2:
+
+      if (items.length === 0) {
+        alert('Nenhum item cadastrado');
+      } else {
+        alert(items);
+      }
+      break;
+
+    case 3:
+
+      if (items.length === 0) {
+        alert('Nenhum item para retirar.');
+      }
+      else {
+        alert(`item retirado: ${items.pop()}`);
+      }
+      break;
 
     case 4:
+
       let question = prompt('Sair do Programa?');
 
       if (question.toLowerCase() === 'sim') {
         alert('Good Bye');
-      } 
+      }
       break;
 
-    default: 
-    alert('Opçao Invalida, Tente novamente')
-  } 
-  
-  console.log(items, option)
-} 
+    default:
+      alert('Opção inválida, tente novamente');
+  }
+
+  console.log(items, option);
+}
+
 
