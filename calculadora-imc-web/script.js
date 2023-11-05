f2a90adc4053d906280ca72d666a74034ddc6248
Kayk eli <kayksantana133@gmail.com>
2023-11-05 08:36:44 -0300

    att

---
+++ calculadora-imc-web/script.js
@@ -0,0 +1,12 @@
+const btnCalcular = document.getElementById('btnCalcular')
+
+
+function calcularIMC() { 
+const peso = document.getElementById('peso').value
+const altura = document.getElementById('altura').value
+
+
+
+}
+
+btnCalcular.addEventListener('click', calcularIMC)
