63cefdb8bde2c80b750e5f33c950c4969c5955ac
Kayk eli <kayksantana133@gmail.com>
2023-11-05 12:39:08 -0300

    att

--- calculadora-imc-web/script.js
+++ calculadora-imc-web/script.js
@@ -1,12 +1,19 @@
 const btnCalcular = document.getElementById('btnCalcular')
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 
 
 function calcularIMC() { 
-const peso = document.getElementById('peso').value
-const altura = document.getElementById('altura').value
+const peso = parseFloat(document.getElementById('peso').value)
+const altura = parseFloat(document.getElementById('altura').value)
 
+const imc = (peso / (altura * altura)).toFixed(2);
 
+screen1.classList.add('hide')
+screen2.classList.remove('hide')
+
+screen2.querySelector('h1').innerHTML = `Seu IMC e de ${imc}`
 
 }
 
-btnCalcular.addEventListener('click', calcularIMC)
+btnCalcular.addEventListener('keydown', calcularIMC)
