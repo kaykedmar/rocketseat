263ccd86dbd2d1f3c4f8082f60e91b5892da6bb0
Kayk eli <kayksantana133@gmail.com>
2023-11-04 21:45:58 -0300

    Update script.js

--- jogo-adivinhacao/script.js
+++ jogo-adivinhacao/script.js
@@ -1,56 +1,49 @@
-const randomNumber = Math.round(Math.random() * 5)
-let trys = 1
-const screen1 = document.querySelector('.screen1')
-const screen2 = document.querySelector('.screen2')
-
-
-// Funçao da pagina 1
-function handleClick(event) { 
-  event.preventDefault()
-  
-  const inputNumber = document.getElementById('inputNumber').value
- 
-  if (Number(inputNumber) == randomNumber) { 
-    screen1.classList.add('hide')
-    screen2.classList.remove('hide')
-
-    document.querySelector('.screen2 h2').innerHTML = `Parabens, Voce acertou em ${trys} tentativa(s) !: `
-
-    console.log(`Parabens, Voce acertou em ${trys} tentativa(s) !: `)
-    trys = 1;
-  }  
-  
-  document.getElementById('inputNumber').value = ''
-  trys++
-  
-  if (inputNumber == '')  { 
-    alert('Error 404: Digite um numero Valido!')
-  }
-
-} 
+//Variaveis
+const randomNumber = Math.round(Math.random() * 10)
 
 const btnTry = document.getElementById('btnTry')
 const btnAgain = document.getElementById('btnAgain')
-
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+let trys = 1
 
 btnTry.addEventListener('click', handleClick)
-btnAgain.addEventListener('click', function() { 
-  screen1.classList.remove('hide')
-  screen2.classList.add('hide')
-})
+btnAgain.addEventListener('click', toggleScreen)
+
+// Funçao da pagina 1
+function handleClick(event) {
+  
+  event.preventDefault()
+  const inputNumber = document.getElementById('inputNumber').value
+
+  if (Number(inputNumber) == randomNumber) {
+    toggleScreen()
+    screen2.querySelector('h2').innerHTML = `Parabens, Voce acertou em ${trys}
+     tentativa(s) !: `
+
+    trys = 1;
+  }
+
+  document.getElementById('inputNumber').value = ''
+  trys++
+
+  if (inputNumber == '') {
+    screen2.querySelector('h2').innerHTML = 'Digite um numero Valido:'
+  }
+}
+
+function handleResetClick() { 
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+  trys = 1
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
 
 
 
-
-
-// Funçao da pagina 2
-// function playAgain(event) { 
-
-//   screen1.classList.remove('hide')
-//   screen2.classList.add('hide')
-
-//   trys = 1;
-
-// }
 
 
