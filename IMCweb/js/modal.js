48f73948880671541af9508c2a76641f5f92bf64
Kayk eli <kayksantana133@gmail.com>
2023-11-13 17:07:11 -0300

    Update modal.js

--- IMCweb/js/modal.js
+++ IMCweb/js/modal.js
@@ -1,19 +1,28 @@
 export const modal = { // objeto literal
-  wrapper: document.querySelector('.modal-wrapper'), 
-  message: document.querySelector('.modal .title span'), 
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
   btnClose: document.querySelector('.modal button.close'),
 
   open() {
     modal.wrapper.classList.add('open')
-   },
+  },
 
-  close() { 
+  close() {
     modal.wrapper.classList.remove('open')
   }
 
 }
 
 // Fechar modal ao clicar no X
-modal.btnClose.onclick = () => { 
+modal.btnClose.onclick = () => {
   modal.close()
 }
+
+
+// Fechar modal pelo teclado
+window.addEventListener('keydown', event => {
+  
+  if(event.key === 'Escape') { 
+    modal.close()
+  }
+})
